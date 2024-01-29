4bbc06140b57a5d922bc51ec38d5587c7eba189b
kundhan <kundhan1997@gmail.com>
2024-01-29 23:23:36 +0530

    configuration changes for open ai and testing

--- ai_model/openai.py
+++ ai_model/openai.py
@@ -2,7 +2,7 @@ from util import read_config
 import requests
 
 config = read_config()
-cred_config = read_config('cred.json')
+cred_config = read_config('../cred.json')
 
 
 def prepare_payload(user_prompt: str):
@@ -13,12 +13,18 @@ def prepare_payload(user_prompt: str):
 
 def prompt_to_json(user_prompt: str):
     url = config['open_ai_url']
-    api_key = cred_config['api_key']
+    api_key = cred_config['open_ai']['api_key']
     headers = config['headers']
     data = prepare_payload(user_prompt)
     response = requests.post(url=url, headers=headers, auth=("", api_key), json=data)
 
     if response.status_code == '200':
         result = response.json
+        print(result)
     else:
         print(f"Error: {response.status_code} - {response.text}")
+
+
+user = "Here is Video foo.mp4. Here is new Audio stream. Clip Video first 3 seconds off video and clip first 3 seconds from Audio. Of the remaining Video and Audio, combine them to form a new Video"
+prompt_to_json(user)
+
