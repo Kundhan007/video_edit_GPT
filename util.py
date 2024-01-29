6e7c7947911e1ab24c3c0e01e30d7678d9f27a8f
kundhan <kundhan1997@gmail.com>
2024-01-29 23:24:40 +0530

    read configuration default changes

--- util.py
+++ util.py
@@ -1,7 +1,7 @@
 import json
 
 
-def read_config(con_file='config.json'):
+def read_config(con_file='../config.json'):
     with open(con_file, 'r') as f:
         config = json.load(f)
     return config
