1e69befa5193561a1a01d8498892a9c071e0b9d7
kundhan <kundhan1997@gmail.com>
2024-01-29 15:30:20 +0530

    request handling skeleton download and upload

---
+++ request_handling/app.py
@@ -0,0 +1,12 @@
+
+
+def video_audio_upload(request_files):
+    video = request_files.video
+    audio = request_files.audio
+    prompt = request_files.prompt
+
+
+    pass
+
+def download_video(video_id):
+    pass
